76108b0092619d23643605052e5e8c0e3deb480a
Fauzaan <mfauzaan@icloud.com>
2022-06-01 16:18:33 +0800

    chore: revert

--- lua/lvim/core/nvimtree.lua
+++ lua/lvim/core/nvimtree.lua
@@ -24,7 +24,6 @@ function M.config()
         enable = true,
         auto_open = true,
       },
-      reload_on_bufenter = true,
       open_on_tab = false,
       hijack_cursor = false,
       update_cwd = false,
